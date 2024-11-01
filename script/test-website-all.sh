ca3478abd63942bdace06a8cb0298dc119ebfd76
Roly Perera <roly.perera@dynamicaspects.org>
2024-11-01 11:55:39 +0000

    🧩 [add-unused]: WEBSITES2.

--- script/test-website-all.sh
+++ script/test-website-all.sh
@@ -3,6 +3,14 @@
 set -xe
 
 set +x
+WEBSITES2=($(for FILE in src/Website/Test/* src/Website/Test/*.purs; do
+    basename "$FILE" | sed 's/\.[^.]*$//'
+done | sort -u))
+set -x
+
+echo "HOW ABOUT THIS: ${WEBSITES2[@]}"
+
+set +x
 WEBSITES=($(for FILE in src/Website/*.{purs,html}; do
    basename "$FILE" | sed 's/\.[^.]*$//'
 done | sort -u))
